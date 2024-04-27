9d1083675380502daf1646621b129113cf020908
miku-wwl <569144003@qq.com>
2024-04-27 22:51:41 +0800

    feat:first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { Ghost } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Root Pages
-      <Button variant="outline">click me</Button>
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex flex-col gap-4">
+        <h1 className="text-center text-xl font-bold">Create App</h1>
+        <Input name="name" placeholder="App Name"></Input>
+        <Textarea name="description" placeholder="Description"></Textarea>
+        <Button type="submit">submit</Button>
+      </form>
     </div>
   );
 }
