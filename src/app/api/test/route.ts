2996313f477d60492f009c504204b089174e6573
miku-wwl <569144003@qq.com>
2024-04-30 00:21:15 +0800

    feat:zod

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,10 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
+import z from "zod";
+
+const inputSchema = z.object({
+  name: z.string().max(10).min(3),
+  email: z.string().email(),
+});
+
 export function GET(request: NextRequest) {
-  console.log("--->");
+  const query = request.nextUrl.searchParams;
+  const name = query.get("name");
+  const email = query.get("email");
 
-  console.log(cookies().get("a"));
+  const result = inputSchema.safeParse({
+    name,
+    email,
+  });
 
-  return NextResponse.json({ a: "Hello World!" });
+  if (result.success) {
+    return NextResponse.json(result.data);
+  } else {
+    return NextResponse.json({ error: result.error.message });
+  }
 }
